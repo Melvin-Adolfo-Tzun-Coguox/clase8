957bc03445a0450a88617f8103cfafaa926580e1
MelvinTC18 <mtzunc@miumg.edu.gt>
2023-04-01 08:47:34 -0600

    Agregar archivos de proyecto.

---
+++ clase8/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+void arreglos()
+{
+    string[] nombre = new string[]
+    {
+        "fernando","melvin","luis","brandon","yamileth"
+    };
+
+    int[] notas = new int[]
+    {
+        70,90,43,85,30
+    };
+    int n1 = 0;
+    int mayor = 0;
+    foreach (int a in notas)
+    {
+        if (a > mayor)
+        {
+            mayor = a;
+            n1++;
+            
+        }
+
+    }
+    Console.WriteLine($"La nota mas alta es de: {nombre[n1-1]} con {mayor} puntos");
+    
+}
+arreglos();
